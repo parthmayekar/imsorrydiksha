f50bde68b8a418c92cd0c379562fc2c0f7800c43
parthmayekar <parth.mayekar.v@gmail.com>
2026-06-27 23:24:34 +0530

    Update HugPage.jsx

--- src/components/HugPage.jsx
+++ src/components/HugPage.jsx
@@ -104,7 +104,7 @@ export default function HugPage({ setCurrentPage }) {
           transition={{ duration: 0.8, delay: 1.1 }}
           className="text-xl text-purple-200 leading-relaxed"
         >
-          miss ur hugs :( </3
+          miss ur hugs :(
         </motion.p>
       </motion.div>
 
